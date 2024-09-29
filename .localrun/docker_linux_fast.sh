a50bb4a453e2bdea9bfe55388ab8e7ba3690000b
zoff99 <zoff99@users.noreply.github.com>
2024-09-29 18:12:04 +0200

    build

--- .localrun/docker_linux_fast.sh
+++ .localrun/docker_linux_fast.sh
@@ -46,16 +46,20 @@ pwd
 ls -al
 id -a
 
+OLDPATH=$PATH
+
+# ------- Android -------
 export ANDROID_NDK_ROOT=/opt/android-sdk/ndk/25.1.8937393
 PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH
-
 archs="android-arm64 android-x86_64 android-x86 android-arm"
 
 for i in $archs; do
     rm -Rf openssl-3.3.2/
     tar -xf /openssl-3.3.2.tar.gz
     cd openssl-3.3.2/
-    ./Configure "$i" -D__ANDROID_API__=21
+    # patch openSSL see: https://github.com/sfackler/rust-openssl/issues/2154 and https://github.com/android/ndk/issues/1992
+    sed -E -i '"'"''"'"' -e '"'"'/[.]hidden.*OPENSSL_armcap_P/d'"'"' -e '"'"'/[.]extern.*OPENSSL_armcap_P/ {p; s/extern/hidden/; }'"'"' crypto/*arm*pl crypto/*/asm/*arm*pl
+    ./Configure no-apps no-docs no-dso no-dgram "$i" -D__ANDROID_API__=21
     make -j $(nproc) || exit 1
     ls -al libcrypto.a libssl.a || exit 1
     file libcrypto.a libssl.a
@@ -64,6 +68,30 @@ for i in $archs; do
     chmod -R a+rw /artefacts/*
     cd ..
 done
+# ------- Android -------
+
+# ------- Linux -------
+i=linux_debian12_x86_64
+export ANDROID_NDK_ROOT=
+PATH=$OLDPATH
+rm -Rf openssl-3.3.2/
+tar -xf /openssl-3.3.2.tar.gz
+cd openssl-3.3.2/
+./Configure no-apps no-docs no-dso no-dgram --prefix=/opt/openssl --openssldir=/usr/local/ssl
+make -j $(nproc) || exit 1
+##### make test
+make install
+ls -al libcrypto.a libssl.a || exit 1
+file libcrypto.a libssl.a
+mkdir -p /artefacts/"$i"/
+cp -av libcrypto.a libssl.a /artefacts/"$i"/ || exit 1
+mkdir -p /artefacts/"$i"/include/
+cp -av /opt/openssl/include/openssl /artefacts/"$i"/include/
+chmod -R a+rw /artefacts/*
+cd ..
+# ------- Linux -------
+
+
 
 ' > "$_HOME_"/"$system_to_build_for"/script/run.sh
 
